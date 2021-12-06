3f874b636441b31aa4f87d58f1451ea4986192ae
RajPathare <rajgpathare@gmail.com>
2021-12-06 18:25:57 +0530

    base code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 
 import { Component } from '@angular/core';
 
-
 @Component({
 
-    selector: 'app-root',
-    template: '<h1>App!</h1>'
+    selector: 'mw-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 
 })
 
